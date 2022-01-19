6d2e5aa10e99a521abb0cd1c391857604495a238
Ryan Burns <ryankburns@gmail.com>
2022-01-18 20:38:58 -0800

    solved

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,6 +1,13 @@
 function selectionSortRecursive(arr) {
   // type your code here
+  if (arr.length < 2) return arr
+  const [p, ...rest] = arr
+  const low  = rest.filter(n => n <= p)
+  const high = rest.filter(n => n > p)
+  return [...selectionSortRecursive(low), p, ...selectionSortRecursive(high)]
 }
+
+
 
 if (require.main === module) {
   // add your own tests in here
